b5bf4fd02b4730db86852fddea8f758fd1cf7ebb
Karthik-Aedumbaka <aedumbakakarthik1@gmail.com>
2026-01-14 19:43:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element/next-greater-element.py
+++ Difficulty: Medium/Next Greater Element/next-greater-element.py
@@ -1,19 +1,34 @@
-from collections import deque
 class Solution:
     def nextLargerElement(self, arr):
         # code here
-        result=[-1]*len(arr)
-        stk = []
-        for i in range(len(arr) - 1, -1, -1):
-            # Remove all smaller or equal elements from the stack
-            while stk and stk[-1] <= arr[i]:
+        # filnd next greatest element 
+        # Brute Force
+        # n = len(arr)
+        # ans=[-1]*n
+        # for i in range(len(arr)):  #6   # fisrt pointer 
+        #     for j in range(i+1,len(arr)):#8 # secound pointer for chack the greater number
+        #         if arr[j] > arr[i]: # checking the condition
+        #             ans[i] = ans[j]
+        #             break # once we file the next greater element break the loop
+        
+        # return ans
+        
+        
+        # 
+        result = [-1]* len(arr)
+        
+        stk = [ ]
+        
+        for i in range(len(arr)-1,-1,-1):
+            while stk and arr[i] >= stk[-1]:
                 stk.pop()
-
-            # If stack is not empty, the top is the next greater element
+                
             if stk:
                 result[i] = stk[-1]
-
-            # Push the current element to stack
+                
+            
             stk.append(arr[i])
-
+        
         return result
+        
+        
